d918643459ad3cf2769a989dda09c4ea4402ce08
polarisdefi <admin@polarisdefi.com>
2021-03-25 07:52:24 +1000

    update to v2

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -16,7 +16,7 @@ const config: MenuEntry[] = [
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.polarisdefi.io/#/add/0xe9e7cea3dedca5984780bafc599bd69add087d56/0x1C545E9943CFd1b41E60a7917465911fa00Fc28C',
+        href: 'https://exchange.polarisdefi.io/#/add/0xe9e7cea3dedca5984780bafc599bd69add087d56/0x3a5325f0e5ee4da06a285e988f052d4e45aa64b4',
       },
     ],
   },
@@ -28,7 +28,7 @@ const config: MenuEntry[] = [
   {
     label: 'Minor Pools',
     icon: 'LaunchIcon',
-    href: 'https://app.polarisdefi.io/nests',
+    href: 'https://app.polarisdefi.io/minor',
   },
   {
     label: 'Supernovas (Coming soon)',
@@ -69,7 +69,7 @@ const config: MenuEntry[] = [
       },
       {
         label: 'BSCScan',
-        href: 'https://bscscan.com/token/0x1C545E9943CFd1b41E60a7917465911fa00Fc28C',
+        href: 'https://bscscan.com/token/0x3a5325f0e5ee4da06a285e988f052d4e45aa64b4',
       },
     ],
   },
--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x1C545E9943CFd1b41E60a7917465911fa00Fc28C',
+  cakeAddress: '0x3a5325f0e5ee4da06a285e988f052d4e45aa64b4',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x66db82fb3e63bd6451ee81b06685abede41232ba'
+  lpAddress: '0xae315a662b7e0cfec68edce6a06fa918b19e3328'
 }
 
 export default priceContracts
